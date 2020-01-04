f909544b7780976b047cf93f74d23bd1f7aa8146
H!Guo <guohai@gmail.com>
2020-01-04 21:31:38 +0800

    V 0.2版本,人物的基本移动和时间日历的显示 已经完成

--- main.c
+++ main.c
@@ -9,11 +9,12 @@
 UINT16 year = 1982;
 UINT8 month = 12;
 UINT8 count = 0;
+UINT8 roleposition = 28;
 
 void calendardisplay(){
         //处理日期
         count++;
-        if(count>=10){
+        if(count>=8){
             count=0;
             month++;
             if(month>=13){
@@ -39,25 +40,27 @@ void main()
     //设置精灵的方向
     INT8 spritediretion=1;
 
-    STAT_REG = 0x45;
-    LYC_REG = 0x08;  //  Fire LCD Interupt on the 8th scan line (just first row)
+    // STAT_REG = 0x45;
+    // LYC_REG = 0x08;  //  Fire LCD Interupt on the 8th scan line (just first row)
     // disable_interrupts();
     SPRITES_8x16;
     //设置图块图案
     set_sprite_data(0, 16, BoyGirl);
     //在子画面上0上，显示 精灵索引
     set_sprite_tile(0, currentspriteindex);
-    move_sprite(0, 28, 136);
+    move_sprite(0, 28, 112);
     set_sprite_tile(1, currentspriteindex+2);
-    move_sprite(1, 28-8, 136);
+    move_sprite(1, 28-8, 112);
     set_sprite_prop(0,S_FLIPX);
     set_sprite_prop(1,S_FLIPX);
     SHOW_SPRITES;
 
     set_bkg_data(0,23,BGTile);
-    // set_win_tiles(0,0,20,3,rcmap);
-    set_bkg_tiles(0,17,20,1,Ground);
-    // SHOW_WIN;
+    set_bkg_tiles(0,0,32,18,Ground);
+
+    set_win_tiles(0,0,20,3,rcmap);
+    move_win(7,120);
+    SHOW_WIN;
     SHOW_BKG;
     DISPLAY_ON;
     
@@ -65,10 +68,11 @@ void main()
     // enable_interrupts();
     // set_interrupts(VBL_IFLAG | LCD_IFLAG); 
     while(1){
-        scroll_bkg(1,0);
+        
         // SHOW_WIN;
+        
         // wait_vbl_done();
-        // calendardisplay();
+        
         //延迟1000毫秒
         delay(100);
         //根据方向键来移动精灵
@@ -81,13 +85,18 @@ void main()
                     scroll_sprite(1,8,0);
                     spritediretion = 2;
                 }
-                scroll_sprite(0,-1,0);
-                scroll_sprite(1,-1,0);
+
                 currentspriteindex = currentspriteindex == 4?8:4;
                 set_sprite_tile(0, currentspriteindex);
                 set_sprite_tile(1, currentspriteindex+2);
                 set_sprite_prop(0,0);
                 set_sprite_prop(1,0);
+                
+                if(roleposition>16){
+                    scroll_sprite(0,-2,0);
+                    scroll_sprite(1,-2,0);
+                    roleposition-=2;
+                }
                 break;
             case J_RIGHT:
                 if(spritediretion==2){
@@ -100,32 +109,42 @@ void main()
                 currentspriteindex = currentspriteindex == 4?8:4;
                 set_sprite_tile(0, currentspriteindex);
                 set_sprite_tile(1, currentspriteindex+2);
-                scroll_sprite(0,1,0);
-                scroll_sprite(1,1,0);
 
+                roleposition+=2;
+                if(roleposition>= 80 ){
+                    scroll_bkg(1,0);
+                }
+                else{
+                    scroll_sprite(0,2,0);
+                    scroll_sprite(1,2,0);
+                }
+                calendardisplay();
                 break;
-            case J_UP:
-                scroll_sprite(0, 0,-1);
-                break;
-            case J_DOWN:
-                scroll_sprite(0,0,1);
-                break;
-            case J_LEFT ^ J_UP:
-                scroll_sprite(0,-1,-1);
-                break;
-            case J_LEFT ^ J_DOWN:
-                scroll_sprite(0, -1, 1);
-                break;
-            case J_RIGHT ^ J_DOWN:
-                scroll_sprite(0, 1,1);
-                break;
-            case J_RIGHT ^ J_UP:
-                scroll_sprite(0,1,-1);
-                break;
+            // case J_UP:
+            //     scroll_sprite(0, 0,-1);
+            //     break;
+            // case J_DOWN:
+            //     scroll_sprite(0,0,1);
+            //     break;
+            // case J_LEFT ^ J_UP:
+            //     scroll_sprite(0,-1,-1);
+            //     break;
+            // case J_LEFT ^ J_DOWN:
+            //     scroll_sprite(0, -1, 1);
+            //     break;
+            // case J_RIGHT ^ J_DOWN:
+            //     scroll_sprite(0, 1,1);
+            //     break;
+            // case J_RIGHT ^ J_UP:
+            //     scroll_sprite(0,1,-1);
+            //     break;
             default:
-                currentspriteindex = 4;
-                set_sprite_tile(0, currentspriteindex);
-                set_sprite_tile(1, currentspriteindex+2);
+                if(currentspriteindex != 4){
+                    currentspriteindex = 4;
+                    set_sprite_tile(0, currentspriteindex);
+                    set_sprite_tile(1, currentspriteindex+2);
+                }
+
                 break;
         }
         
